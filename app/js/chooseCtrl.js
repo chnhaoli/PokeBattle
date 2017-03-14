364d89b34ff3f97d599df739b393739c4da86857
Beichen Chen <beichen@kth.se>
2017-03-14 14:19:40 +0100

    first commit

--- app/js/chooseCtrl.js
+++ app/js/chooseCtrl.js
@@ -42,6 +42,64 @@ pokeBattleApp.controller('ChooseCtrl', function ($scope, $uibModal, $log, dialog
   }
 
 
+  // Drag and drop - testing
+  /*$scope.models = {
+       selected: null,
+       lists: {"A": [], "B": []}
+   };
+
+   // Generate initial model
+   for (var i = 1; i <= 3; ++i) {
+       $scope.models.lists.A.push({label: "Item A" + i});
+       $scope.models.lists.B.push({label: "Item B" + i});
+   }
+
+   // Model to JSON for demo purpose
+   $scope.$watch('models', function(model) {
+       $scope.modelAsJson = angular.toJson(model, true);
+   }, true);*/
+
+   $scope.dragoverCallback = function(index, external, type, callback) {
+        $scope.logListEvent('dragged over', index, external, type);
+        // Invoke callback to origin for container types.
+        if (type == 'container' && !external) {
+            console.log('Container being dragged contains ' + callback() + ' items');
+        }
+        return index < 10; // Disallow dropping in the third row.
+    };
+
+    $scope.dropCallback = function(index, item, external, type) {
+        $scope.logListEvent('dropped at', index, external, type);
+        // Return false here to cancel drop. Return true if you insert the item yourself.
+        return item;
+    };
+
+    $scope.logEvent = function(message) {
+        console.log(message);
+    };
+
+    $scope.logListEvent = function(action, index, external, type) {
+        var message = external ? 'External ' : '';
+        message += type + ' element was ' + action + ' position ' + index;
+        console.log(message);
+    };
+
+    // Initialize model
+    $scope.model = [[], []];
+    var id = 10;
+    angular.forEach(['all', 'move', 'copy', 'link', 'copyLink', 'copyMove'], function(effect, i) {
+      var container = {items: [], effectAllowed: effect};
+      for (var k = 0; k < 7; ++k) {
+        container.items.push({label: effect + ' ' + id++, effectAllowed: effect});
+      }
+      $scope.model[i % $scope.model.length].push(container);
+    });
+
+    $scope.$watch('model', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+    }, true);
+
+
   // Modal popup - testing
 
   var $ctrl = this;
