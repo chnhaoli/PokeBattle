9dcbffc38a36a4280f6bf750b609031aff3483bb
chnhaoli <chn.haoli@gmail.com>
2017-03-29 16:38:55 +0200

    delete test

--- app/js/chooseCtrl.js
+++ app/js/chooseCtrl.js
@@ -202,32 +202,3 @@ pokeBattleApp.controller('ModalInstanceCtrl', function ($uibModalInstance, pokem
         $uibModalInstance.dismiss('cancel');
     };
 });
-
-
-//progressbar controller
-pokeBattleApp.controller('ProgressDemoCtrl', function ($scope) {
-    $scope.max = 200;
-
-    $scope.random = function() {
-        var value = Math.floor(Math.random() * 100 + 1);
-        var type = 'info';
-        $scope.type = type;
-    };
-
-    $scope.random();
-
-    $scope.randomStacked = function() {
-        $scope.stacked = [];
-        var types = ['success', 'info', 'warning', 'danger'];
-
-        for (var i = 0, n = Math.floor(Math.random() * 4 + 1); i < n; i++) {
-            var index = Math.floor(Math.random() * 4);
-            $scope.stacked.push({
-                value: Math.floor(Math.random() * 30 + 1),
-                type: types[index]
-            });
-        }
-    };
-
-    $scope.randomStacked();
-});
